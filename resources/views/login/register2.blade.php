ba1655b1fc6882b26b16912640a79063bc96f522
zaidan20ti <zaidan20ti@mahasiswa.pcr.ac.id>
2021-12-03 11:55:35 +0700

    Commited'

--- resources/views/login/register2.blade.php
+++ resources/views/login/register2.blade.php
@@ -37,7 +37,7 @@ License: For each use you must have a valid license purchased only from above li
 				<div class="d-flex flex-center flex-column flex-column-fluid p-10 pb-lg-20">
 					<!--begin::Logo-->
 					<a href="../../demo9/dist/index.html" class="mb-12">
-						<img alt="Logo" src="assets/media/logos/logo-1.svg" class="h-80px" />
+						<!-- <img alt="Logo" src="assets/media/logos/logo-1.svg" class="h-80px" /> -->
 					</a>
 					<!--end::Logo-->
 					<!--begin::Wrapper-->
