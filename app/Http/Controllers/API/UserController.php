d4eb3aa17472e9f782de959c24348dccdd5ba841
zaidan20ti <zaidan20ti@mahasiswa.pcr.ac.id>
2022-02-21 12:35:02 +0700

    API Profile Update

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -70,6 +70,8 @@ class UserController extends Controller
             ->where("id_user", $user["id_user"])
             ->where("progress", "!=", "100")->get()->toArray();
 
+        $classCompleted = CompletedClass::where("id_user", $user["id_user"])->get()->toArray();
+
         $temp = [];
         if(count($classProgress) > 0) {
             foreach($classProgress as $class) {
@@ -80,13 +82,24 @@ class UserController extends Controller
             }
         }
 
+        $temp2 = [];
+        if(count($classCompleted) > 0) {
+            foreach($classCompleted as $class) {
+                $class_temp = Kelas::select("id_kelas", "gambar", "judul")->where("id_kelas", $class["id_kelas"])->first()->toArray();
+                $class_temp["days_left"] = 59;
+                $class_temp["progress"] = $class["progress"];
+                array_push($temp2, $class_temp);
+            }
+        }
+
         $profile = [
             "name" => $user["name"],
             "email" => $user["email"],
             "joined" => "Joined since " . Helper::time_elapsed_string($user["created_at"]),
             "class_on_progress" => count($classProgress),
-            "completed_classes" => count(CompletedClass::where("id_user", $user["id_user"])->get()->toArray()),
-            "class_progress" => $temp
+            "completed_classes" => count($classCompleted),
+            "class_progress" => $temp,
+            "class_completed" => $temp2
         ];
 
         
