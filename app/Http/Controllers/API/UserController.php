66bd54911aa78d2f0ba38a75d1249d7de5597713
Mahrus Zain <mahrus@pcr.ac.id>
2022-02-24 06:17:56 +0000

    Committed

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -85,7 +85,7 @@ class UserController extends Controller
         $temp2 = [];
         if(count($classCompleted) > 0) {
             foreach($classCompleted as $class) {
-                $class_temp = Kelas::select("id_kelas", "gambar", "judul")->where("id_kelas", $class["id_kelas"])->first()->toArray();
+                $class_temp = Kelas::select("id_kelas", "created_at", "gambar", "judul")->where("id_kelas", $class["id_kelas"])->first()->toArray();
                 array_push($temp2, $class_temp);
             }
         }
