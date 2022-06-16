104d1417d8cd935b30e1ab3d39538d7fa619510d
Zaidan Anwar <zaidan20ti@mahasiswa.pcr.ac.id>
2022-06-16 15:49:56 +0700

    Adding Migration,Seeder and Model

---
+++ database/migrations/2022_06_16_083422_learning_path.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class LearningPath extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::create('learning_path', function (Blueprint $table) {
+            $table->increments('id_learning_path');
+            $table->string("name");
+            $table->string("desc");
+            $table->timestamps();
+        });
+    }
+}
---
+++ database/migrations/2022_06_16_084001_learning_path_class.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class LearningPathClass extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('learning_path_class', function (Blueprint $table) {
+            $table->increments('id_learning_path_class');
+            $table->increments('id_learning_path');
+            $table->increments('id_learning_kelas');
+            $table->timestamps();
+            
+            $table->foreign('id_learning_path')->references('id_learning_path')->on('learning_path');
+            $table->foreign('id_kelas')->references('id_kelas')->on('kelas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
