1bdca42106a1d5bec7d6e3665409cb76fb6f717b
Zaidan Anwar <zaidan20ti@mahasiswa.pcr.ac.id>
2022-07-02 11:51:12 +0700

    Committed

--- database/seeders/Kelas_userSeeder.php
+++ database/seeders/Kelas_userSeeder.php
@@ -18,7 +18,43 @@ class Kelas_userSeeder extends Seeder
     {
         DB::table('kelas_user')->insert([
             'id_kelas' => random_int(5, 6),
-            'id_user' => 1,
+            'id_user' => 2,
+            'point_review' => random_int(4, 5),
+            'komentar_review' => "The mentor is cool, the way he conv-eys each material is also detailed and easy to reach and the featured really make easy to learn arabic. Words can not describe this class!",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+        
+        DB::table('kelas_user')->insert([
+            'id_kelas' => random_int(5, 6),
+            'id_user' => 3,
+            'point_review' => random_int(4, 5),
+            'komentar_review' => "The mentor is cool, the way he conv-eys each material is also detailed and easy to reach and the featured really make easy to learn arabic. Words can not describe this class!",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+        
+        DB::table('kelas_user')->insert([
+            'id_kelas' => random_int(5, 6),
+            'id_user' => 4,
+            'point_review' => random_int(4, 5),
+            'komentar_review' => "The mentor is cool, the way he conv-eys each material is also detailed and easy to reach and the featured really make easy to learn arabic. Words can not describe this class!",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+        
+        DB::table('kelas_user')->insert([
+            'id_kelas' => random_int(5, 6),
+            'id_user' => 5,
+            'point_review' => random_int(4, 5),
+            'komentar_review' => "The mentor is cool, the way he conv-eys each material is also detailed and easy to reach and the featured really make easy to learn arabic. Words can not describe this class!",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+        
+        DB::table('kelas_user')->insert([
+            'id_kelas' => random_int(5, 6),
+            'id_user' => 6,
             'point_review' => random_int(4, 5),
             'komentar_review' => "The mentor is cool, the way he conv-eys each material is also detailed and easy to reach and the featured really make easy to learn arabic. Words can not describe this class!",
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
