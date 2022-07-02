6903f0046a4ecb94911ecb43040b4e9ae90718e7
Zaidan Anwar <zaidan20ti@mahasiswa.pcr.ac.id>
2022-07-02 11:32:45 +0700

    Committed

--- database/seeders/Kelas_userSeeder.php
+++ database/seeders/Kelas_userSeeder.php
@@ -17,7 +17,7 @@ class Kelas_userSeeder extends Seeder
     public function run()
     {
         DB::table('kelas_user')->insert([
-            'id_kelas' => random_int(1, 2),
+            'id_kelas' => random_int(5, 6),
             'id_user' => 1,
             'point_review' => random_int(4, 5),
             'komentar_review' => "The mentor is cool, the way he conv-eys each material is also detailed and easy to reach and the featured really make easy to learn arabic. Words can not describe this class!",
