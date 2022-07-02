a3b1f235cca2fd25161e86ef73881058b71c4489
Zaidan Anwar <zaidan20ti@mahasiswa.pcr.ac.id>
2022-07-02 11:53:01 +0700

    Committed

--- database/seeders/Kelas_userSeeder.php
+++ database/seeders/Kelas_userSeeder.php
@@ -29,7 +29,7 @@ class Kelas_userSeeder extends Seeder
             'id_kelas' => random_int(5, 6),
             'id_user' => 3,
             'point_review' => random_int(4, 5),
-            'komentar_review' => "The mentor is cool, the way he conv-eys each material is also detailed and easy to reach and the featured really make easy to learn arabic. Words can not describe this class!",
+            'komentar_review' => "Very useful classes, up to date know-ledge and most importantly free of charge. Words can not describe how grateful I am for finding this website. Amazing for awesome.",
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
         ]);
@@ -38,7 +38,7 @@ class Kelas_userSeeder extends Seeder
             'id_kelas' => random_int(5, 6),
             'id_user' => 4,
             'point_review' => random_int(4, 5),
-            'komentar_review' => "The mentor is cool, the way he conv-eys each material is also detailed and easy to reach and the featured really make easy to learn arabic. Words can not describe this class!",
+            'komentar_review' => "The most recommended class is basi-cally for investing in the knowledge of the hereafter and suitable for learning Arabic knowledge quickly and easily, thankyour for my mentor!",
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
         ]);
@@ -47,7 +47,7 @@ class Kelas_userSeeder extends Seeder
             'id_kelas' => random_int(5, 6),
             'id_user' => 5,
             'point_review' => random_int(4, 5),
-            'komentar_review' => "The mentor is cool, the way he conv-eys each material is also detailed and easy to reach and the featured really make easy to learn arabic. Words can not describe this class!",
+            'komentar_review' => "So very useful classes, up to date know-ledge and most importantly free of charge. Words can not describe how grateful I am for finding this website. Amazing for awesome.",
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
         ]);
@@ -56,7 +56,7 @@ class Kelas_userSeeder extends Seeder
             'id_kelas' => random_int(5, 6),
             'id_user' => 6,
             'point_review' => random_int(4, 5),
-            'komentar_review' => "The mentor is cool, the way he conv-eys each material is also detailed and easy to reach and the featured really make easy to learn arabic. Words can not describe this class!",
+            'komentar_review' => "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teac of the great explorer of the truth, the master-builder of human happiness. No one rejects.",
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
         ]);
