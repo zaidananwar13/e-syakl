79570badf087336743ee828085ce55bab21dae91
Zaidan Anwar <zaidan20ti@mahasiswa.pcr.ac.id>
2022-07-02 11:55:54 +0700

    Committed

--- database/seeders/LearningPathSeeder.php
+++ database/seeders/LearningPathSeeder.php
@@ -21,7 +21,7 @@ class LearningPathSeeder extends Seeder
         DB::table('learning_path')->insert([
             'id_learning_path' => 1,
             'name' => "Nahwu Class",
-            'desc' => "Dummy Desc",
+            'desc' => "Language is very important and the impression that Nahwu is a difficult science to learn, especially by Muslims. As one of the basic sciences in Arabic",
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
         ]);
@@ -29,7 +29,7 @@ class LearningPathSeeder extends Seeder
         DB::table('learning_path')->insert([
             'id_learning_path' => 2,
             'name' => "Shorof Class",
-            'desc' => "Dummy Desc",
+            'desc' => "Shorof is one of the branches of knowledge in Arabic which studies about changing the shape of a word in Arabic",
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
         ]);
@@ -37,7 +37,7 @@ class LearningPathSeeder extends Seeder
         DB::table('learning_path')->insert([
             'id_learning_path' => 3,
             'name' => "Kaligrafi Class",
-            'desc' => "Dummy Desc",
+            'desc' => "In the book Calligraphy Art (1985) by Abdul Karim Husain, the word calligraphy comes from Latin which consists of kalios (calios) which means beautiful and graf (graph) which means drawing or writing in Arabic.",
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
         ]);
