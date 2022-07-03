6311a4622e1cf9008f0e64f448360225e7e281ae
Zaidan Anwar <zaidan20ti@mahasiswa.pcr.ac.id>
2022-07-03 12:57:41 +0700

    Certificate

--- database/seeders/CertificateSeeder.php
+++ database/seeders/CertificateSeeder.php
@@ -17,8 +17,8 @@ class CertificateSeeder extends Seeder
     public function run()
     {
         DB::table('certificate')->insert([
-            'id_user' => 1,
-            'id_kelas' => 1,
+            'id_user' => 3,
+            'id_kelas' => 5,
             'token' => hash('sha256', Str::random(60)),
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
